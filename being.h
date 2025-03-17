63f05fc2cec651ab248c84d6b03cf177419924b3
kristianq <kristian.quick@bahnhof.net>
2025-03-17 16:19:47 +0100

    dev

--- being.h
+++ being.h
@@ -4,7 +4,7 @@
 typedef enum heading { UNDERVALUE2 = -2, UNDERVALUE1, UP, UPRIGHT, RIGHT, DOWNRIGHT, DOWN, DOWNLEFT, LEFT, UPLEFT, OVERVALUE1, OVERVALUE2 } Heading;
 typedef enum object { NONE, OTHERBEING, FENCE } Object;
 typedef enum myColor { GREEN = 2, BLUE = 4 } MyColor;
-typedef enum fightevent { NONE, STRIKE, DAMAGETAKING } Fightevent; 
+typedef enum fightevent { NONE, STALKING, STRIKING, DAMAGETAKING } Fightevent; 
 typedef struct sight {
 	 Object leftnear;
 	 Object leftfar;
@@ -34,6 +34,7 @@ void beingToPrint(const Being *beingToPrint);
 int spawnBeing(Being *beingToGiveLife, const int *beingNbr, const MyColor *myColor);
 void movement(Being *beingToTurn);
 void turnBeing(Being *beingToTurn, const int *beingNbr);
+bool isEnemy(Being *beingMe, const int *targetx, const int *targety);
 
 #endif
 
