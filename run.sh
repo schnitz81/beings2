b130a73d23b9c020fddf8fb69f83fa2eb8ac1f58
schnitz81 <k.quick81@gmail.com>
2025-04-08 15:59:46 +0200

    updated run script

--- run.sh
+++ run.sh
@@ -1,33 +1,37 @@
-#if ! [ -e beings ]; then
-	echo "No executable found. Preparing to build..."; sleep .1
-	echo -n "Checking gcc..."; sleep .1
+if ! [ -e beings ]; then
+	echo "No executable found. Preparing to build..." 
+	echo -n "Checking gcc..."
 	if [ -z "$(which gcc)" ]; then
 		echo -e "failed!\n\nMake sure gcc is installed.\n"
 		exit 1
 	fi
+	echo "success!";
 
+	echo -n "Checking curses..."
+	if [[ ! -n $(find /usr/* -name 'ncurses.h') ]]; then
+		echo -e "failed!\n\nncurses.h not found. Make sure ncurses package is installed.\n"
+		exit 1
+	fi
+	echo "success!"
 
-    echo "Proceeding to build executable..."; sleep .1
+    echo "Proceeding to build executable..."
 
 	echo 'Building...'
-	gcc -g -v -Wfatal-errors -Wall -c main.c &&
-	gcc -g -v -Wfatal-errors -Wall -c world.c &&
-	gcc -g -v -Wfatal-errors -Wall -c ai.c &&
-	gcc -g -v -Wfatal-errors -Wall -c event.c &&
-	gcc -g -v -Wfatal-errors -Wall -c being.c &&
-	gcc -g -v -Wfatal-errors -o beings main.o world.o ai.o event.o being.o -lncurses -ltinfo 
+	gcc -Wfatal-errors -Wall -c main.c &&
+	gcc -Wfatal-errors -Wall -c world.c &&
+	gcc -Wfatal-errors -Wall -c ai.c &&
+	gcc -Wfatal-errors -Wall -c event.c &&
+	gcc -Wfatal-errors -Wall -c being.c &&
+	gcc -Wfatal-errors -o beings main.o world.o ai.o event.o being.o -lncurses -ltinfo 
 
 	if [ $? -eq 0 ]; then
-		echo "Build successful."
-		#./beings
+		echo "Build successful. Starting executable..."
+		./beings
 	else
-		echo "Build failed."
+		echo "Build failed. Unable to start."
 	fi
 	
-#else
-#	echo -e "\nExecutable found. Starting..."; sleep 1
-	#./beings
-#fi
-
-
-
+else
+	echo -e "\nExecutable found. Starting..."
+	./beings
+fi
